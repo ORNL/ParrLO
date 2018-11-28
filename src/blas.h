98b0a685b28f016c56d3c90377f6bf8670f583bd
Jean-Luc Fattebert <fattebertj@ornl.gov>
2018-11-28 13:47:39 -0500

    Initial code to test build with BLAS

---
+++ src/blas.h
@@ -0,0 +1,10 @@
+extern "C"
+{
+
+void C_DSCAL(
+    const int *n,
+    const double *a,
+    double *x,
+    const int *incx);
+
+}
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+#include "blas.h"
+
 int main(int argc, char** argv)
 {
+    // read in an int (dimension)
+    const int n = atoi( argv[1] );
+
+    // build vector of values
+    std::vector<double> v1(n);
+    for (auto& it : v1)
+    {
+        it = 1.5;
+    }
+
+    //blas1 call
+    int ione = 1;
+    double alpha = 2.;
+    C_DSCAL(&n, &alpha, &v1[0], &ione);
+
+    // print out rescaled vector
+    for (auto& it : v1)
+    {
+        std::cout<< it << std::endl;
+    }
+
     return 0;
 }
 
