35b7073eda3028e62633e3af047b4b597f9ac733
Capone L <lcapone@or-condo-login02.ornl.gov>
2019-06-17 16:35:28 -0400

    matrix example fixed

--- src/matrix_example.cc
+++ src/matrix_example.cc
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 
         int i = MPI_Init(&argc,&argv);
  
-        if( i != MPI_SUCCESS)
+        if( i != MPI_SUCCESS){
         }else{
 
         std::cout<<"MPI SUCCESS"<<i<<std::endl;
