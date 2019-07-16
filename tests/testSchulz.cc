f24dbb44818cb18f0d0726793903fdb690c17d28
Fattebert, Jean-Luc <fattebertj@ornl.gov>
2019-07-16 16:24:10 -0400

    Implement diagonalization of Replicated matrix using MAGMA syevd

---
+++ tests/testSchulz.cc
@@ -0,0 +1,105 @@
+#include "MatrixClasses/Replicated_decl.hpp"
+
+#ifdef USE_MAGMA
+#include "magma_v2.h"
+#endif
+
+#include <iostream>
+#include <vector>
+#include <mpi.h>
+
+int main(int argc, char** argv)
+{
+    std::cout<<"Test Schulz iterative solver"<<std::endl;
+
+    int mpirc = MPI_Init(&argc, &argv);
+    if (mpirc != MPI_SUCCESS)
+    {
+        std::cerr << "MPI Initialization failed!!!" << std::endl;
+        return 1;
+    }
+
+    magma_int_t magmalog = magma_init();
+    if (magmalog == MAGMA_SUCCESS)
+    {
+        std::cout<<"MAGMA INIT SUCCESS"<<std::endl;
+    }else{
+        if (magmalog == MAGMA_ERR_UNKNOWN)
+            std::cout<<"MAGMA INIT FAILS UNKNOWN ERROR"<<std::endl;
+        if (magmalog == MAGMA_ERR_HOST_ALLOC)
+            std::cout<<"MAGMA INIT FAILS HOST ALLOC"<<std::endl;
+        return 1;
+    }
+
+    // dimension of matrix
+    const int n = 10;
+
+    Replicated A(n, MPI_COMM_WORLD);
+
+    //initialize with random values in interval [-0.1,0.1]
+    A.initializeRandomSymmetric();
+    A.scale(0.1);
+
+    Replicated B(n, MPI_COMM_WORLD);
+    B.setDiagonal(1.);    
+    B.add(0.1, A);
+    B.printMatrix();
+
+    //C is a copy of B
+    Replicated C(B);
+
+    C.InvSqrt();
+    const double normC = C.maxNorm();
+    if(std::isnan(normC))
+    {
+        std::cout<<"Max Norm of C is NaN!!!"<<std::endl;
+        return 1;
+    }
+
+    B.SchulzCoupled(20, 1.e-6);
+
+    B.add(-1., C);
+
+    const double normdiff = B.maxNorm();
+    std::cout<<"Norm Difference: "<<normdiff<<std::endl;
+    if(std::isnan(normdiff))
+    {
+        std::cout<<"Difference is NaN!!!"<<std::endl;
+        return 1;
+    }
+
+    const double tol = 1.e-6;
+    std::cout<<"Difference:\n";
+    B.printMatrix();
+
+    if( normdiff>tol )
+    {
+        std::cout<<"Difference larger than tol!!!"<<std::endl;
+        return 1;
+    }
+
+    magmalog = magma_finalize();
+
+    if (magmalog == MAGMA_SUCCESS)
+    {
+     std::cout<<"MAGMA FINALIZE SUCCESS"<<std::endl;
+    }else{
+     if (magmalog == MAGMA_ERR_UNKNOWN)
+        std::cout<<"MAGMA FINALIZE FAILS UNKNOWN ERROR"<<std::endl;
+     if (magmalog == MAGMA_ERR_HOST_ALLOC)
+        std::cout<<"MAGMA FINALIZE FAILS HOST ALLOC"<<std::endl;
+       return 1; 
+    }
+
+    std::cout<<"TEST SUCCESSFUL"<<std::endl;
+
+    mpirc = MPI_Finalize();
+    if (mpirc != MPI_SUCCESS)
+    {
+        std::cerr << "MPI_Finalize() failed!!!" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
+
