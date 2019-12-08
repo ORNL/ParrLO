39dbddb8419d6d3aa2406cf19660f0edc266da7e
Lupo Pasini, Massimiliano <lupopasinim@ornl.gov>
2019-12-08 16:09:16 -0500

    Removed addition of identity in testOrthoGaussian and testOrthoHatFunction

--- tests/testOrthoGaussian.cc
+++ tests/testOrthoGaussian.cc
@@ -48,13 +48,6 @@ int main(int argc, char** argv)
         Matrix B(nrows, ncols, lacomm);
 
         A.gaussianColumnsInitialize(0.8);
-        A.scaleMatrix(0.01);
-        A.printMatrix();
-
-        B.identityInitialize();
-        B.printMatrix();
-
-        A.matrixSum(B);
         A.printMatrix();
 
         double dfo_before = 0.0;
